05b3a266c09b749658a1d64e35803077a4fdb650
Gabriel Hiro Furukawa <71856683+GabrielHiro@users.noreply.github.com>
2022-08-28 14:00:33 -0300

    Add files via upload

--- JS/logicas.js
+++ JS/logicas.js
@@ -2,6 +2,14 @@
 
 var a = 0; /* não alterar o nome */
 
+var b = 0; /* não alterar o nome */
+
+var c = 0;
+/* BOTÃO IN/OUT */
+var MainMenu = document.getElementById('btn-MainMenu');
+var MainMenuOpen = document.querySelector('.e6_59');
+var MainMenuColor = document.querySelector('.e5_125');
+
 /* BOTÃO IN/OUT */
 var btn = document.getElementById('btn-InOut');
 var InOutOpen = document.querySelector('.e6_58');
@@ -11,15 +19,20 @@ var Control = document.getElementById('btn-control');
 var ControlOpen = document.querySelector('.group_30_203_3');
 var ControlColor = document.querySelector('.e6_9');
 
-/* BOTÃO CONTROL */
+/* BOTÃO DEVICE */
 var Device = document.getElementById('btn-device');
 var DeviceOpen = document.querySelector('.group_90_204_111');
 var DeviceColor = document.querySelector('.e6_27');
 
+/* BOTÃO MOTION */
+var Motion = document.getElementById('btn-motion');
+var MotionOpen = document.querySelector('.br_motion_205_148');
+var MotionColor = document.querySelector('.e6_31');
+
 
 //FUNCÕES
-function IfandElse(varOn,varColor) {
-      if(varOn.style.display === 'block' &&  a === 1) {
+function main(varOn,varColor) {
+      if(varOn.style.display === 'block' &&  a === 1 && b === 0) {
             varOn.style.display = 'none';
             varColor.style.background = 'rgba(104.12499949336052, 103.95144760608673, 103.95144760608673, 1)';
             a = 0;
@@ -29,7 +42,22 @@ function IfandElse(varOn,varColor) {
             a = 1
         }
 }
+function IfandElse(varOn,varColor) {
+      if(varOn.style.display === 'block' &&  b === 1) {
+            varOn.style.display = 'none';
+            varColor.style.background = 'rgba(104.12499949336052, 103.95144760608673, 103.95144760608673, 1)';
+            b = 0;
+        } else if(b === 0){
+            varOn.style.display = 'block';
+            varColor.style.background = 'rgba(118.30300390720367, 96.05002999305725, 255, 1)';
+            b = 1
+        }
+}
 
+/* BOTÃO IN/OUT */
+MainMenu.addEventListener('click', function() {
+      main(MainMenuOpen,MainMenuColor)
+});
 /* BOTÃO IN/OUT */
 btn.addEventListener('click', function() {
       IfandElse(InOutOpen,InOutColor)
@@ -43,4 +71,8 @@ Control.addEventListener('click', function() {
 /* BOTÃO DEVICE */
 Device.addEventListener('click', function() {
       IfandElse(DeviceOpen,DeviceColor)
+});
+/* BOTÃO DEVICE */
+Motion.addEventListener('click', function() {
+      IfandElse(MotionOpen,MotionColor)
 });
