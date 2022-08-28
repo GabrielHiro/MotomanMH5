a75f382fb6ae84b334d3fa9ee7d11152e6b27725
Gabriel Hiro Furukawa <71856683+GabrielHiro@users.noreply.github.com>
2022-08-28 13:28:19 -0300

    Add files via upload
    BOTÕES COR

--- JS/logicas.js
+++ JS/logicas.js
@@ -1,26 +1,46 @@
+//DECLARAÇÃO DAS VARIAVEIS
+
+var a = 0; /* não alterar o nome */
+
+/* BOTÃO IN/OUT */
 var btn = document.getElementById('btn-InOut');
 var InOutOpen = document.querySelector('.e6_58');
-var i = 0;
+var InOutColor = document.querySelector('.e5_104');
+/* BOTÃO CONTROL */
 var Control = document.getElementById('btn-control');
 var ControlOpen = document.querySelector('.group_30_203_3');
-var a = 0;
-btn.addEventListener('click', function() {
-    
-  if(InOutOpen.style.display === 'block' && a === 0) {
-      InOutOpen.style.display = 'none';
-      i = 0;
-  } else if(a === 0){
-      InOutOpen.style.display = 'block';
-      i = 1
-  }
-});
-Control.addEventListener('click', function() {
-    
-      if(ControlOpen.style.display === 'block' && i === 0) {
-            ControlOpen.style.display = 'none';
+var ControlColor = document.querySelector('.e6_9');
+
+/* BOTÃO CONTROL */
+var Device = document.getElementById('btn-device');
+var DeviceOpen = document.querySelector('.group_90_204_111');
+var DeviceColor = document.querySelector('.e6_27');
+
+
+//FUNCÕES
+function IfandElse(varOn,varColor) {
+      if(varOn.style.display === 'block' &&  a === 1) {
+            varOn.style.display = 'none';
+            varColor.style.background = 'rgba(104.12499949336052, 103.95144760608673, 103.95144760608673, 1)';
             a = 0;
-      } else if(i === 0){
-            ControlOpen.style.display = 'block';
+        } else if(a === 0){
+            varOn.style.display = 'block';
+            varColor.style.background = 'rgba(118.30300390720367, 96.05002999305725, 255, 1)';
             a = 1
-      }
-    });
+        }
+}
+
+/* BOTÃO IN/OUT */
+btn.addEventListener('click', function() {
+      IfandElse(InOutOpen,InOutColor)
+});
+
+/* BOTÃO CONTROL */
+Control.addEventListener('click', function() {
+      IfandElse(ControlOpen,ControlColor)
+});
+
+/* BOTÃO DEVICE */
+Device.addEventListener('click', function() {
+      IfandElse(DeviceOpen,DeviceColor)
+});
