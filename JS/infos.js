e666e251be0b1865aa794894c2a98cda84f53e6e
Gabriel Hiro Furukawa <hirogamer10@gmail.com>
2025-03-15 21:37:12 -0300

    refector: melhora as funções de alternância de visibilidade e melhora a legibilidade do código

--- JS/infos.js
+++ JS/infos.js
@@ -1,49 +1,53 @@
+"use strict";
+
 // INFORMAÇÕES SOBRE OS BOTÕES
-// DECLARAÇÃO DAS VARIAVEIS
+// DECLARAÇÃO DAS VARIÁVEIS
 
 // INICIO VAR IN/OUT
-
 var btn = document.getElementById('btn-InOut');
 var Dout = document.querySelector('.e6_5');
 var DoutInfos = document.querySelector('.infos_dout');
-
-
 var Wait = document.querySelector('.e6_11');
 var WaitInfos = document.querySelector('.infos_wait');
 
+// Variável de controle para alternar a visibilidade
+var c = 0;
 
-//FUNÇÕES
+// FUNÇÕES
 
-function MouseSee(varOn) {
-    if(varOn.style.display === 'block' &&  c === 1){
-          varOn.style.display = 'none';
-          c = 0;
-      } else if(c === 0){
-          varOn.style.display = 'block';
-          c = 1
-      }
+/**
+ * Alterna a visibilidade de um elemento HTML com base no estado atual e na variável de controle `c`.
+ *
+ * @param {HTMLElement} element - O elemento HTML cuja visibilidade será alternada.
+ */
+function alternarVisibilidade(element) {
+    if (element.style.display === 'block' && c === 1) {
+        element.style.display = 'none';
+        c = 0;
+    } else if (c === 0) {
+        element.style.display = 'block';
+        c = 1;
+    }
 }
 
+/**
+ * Adiciona eventos de mouseover e mouseout para alternar a visibilidade das informações.
+ *
+ * @param {HTMLElement} triggerElement - O elemento que irá disparar os eventos.
+ * @param {HTMLElement} targetElement - O elemento cuja visibilidade será alternada.
+ */
+function adicionarEventosVisibilidade(triggerElement, targetElement) {
+    triggerElement.addEventListener('mouseover', function() {
+        alternarVisibilidade(targetElement);
+    });
+    triggerElement.addEventListener('mouseout', function() {
+        alternarVisibilidade(targetElement);
+    });
+}
 
 // INFORMAÇÕES BOTÕES
 
-btn.addEventListener('mouseover', function() {
-    MouseSee(InOutInfos)
-});
-btn.addEventListener('mouseout', function() {
-    MouseSee(InOutInfos)
-});
-
-Dout.addEventListener('mouseover', function() {
-    MouseSee(DoutInfos)
-});
-Dout.addEventListener('mouseout', function() {
-    MouseSee(DoutInfos)
-});
-
-Wait.addEventListener('mouseover', function() {
-    MouseSee(WaitInfos)
-});
-Wait.addEventListener('mouseout', function() {
-    MouseSee(WaitInfos)
-});
+// Adiciona eventos de mouseover e mouseout para alternar a visibilidade das informações
+adicionarEventosVisibilidade(btn, InOutInfos);
+adicionarEventosVisibilidade(Dout, DoutInfos);
+adicionarEventosVisibilidade(Wait, WaitInfos);
